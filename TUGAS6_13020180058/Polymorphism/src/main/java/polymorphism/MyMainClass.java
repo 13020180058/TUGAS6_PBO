c1cbed2d2b9a5a53a332ea3826005257edc70841
Apriani magfira <57305690+13020180058@users.noreply.github.com>
2020-04-21 16:36:17 +0800

    Add files via upload

---
+++ TUGAS6_13020180058/Polymorphism/src/main/java/polymorphism/MyMainClass.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polymorphism;
+
+/**
+ *
+ * @author ASUS-PC apriani magfira
+ */
+
+
+class MyMainClass {
+  public static void main(String[] args) {
+    Animal myAnimal = new Animal();  // Create a Animal object
+    Animal myPig = new Pig();  // Create a Pig object
+    Animal myDog = new Dog();  // Create a Dog object
+    myAnimal.animalSound();
+    myPig.animalSound();
+    myDog.animalSound();
+  }
+}
+
